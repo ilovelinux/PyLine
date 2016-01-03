6d0722933f51a9234736d071e976d37d3ff9f656
ilovelinux <antoniospadaro45@gmail.com>
2016-01-03 17:01:28 +0100

    Fix identation

--- static/scripts/interpreter.js
+++ static/scripts/interpreter.js
@@ -15,24 +15,24 @@ $(function() {
             $('#interprete').val('');
             cmd.push(text);
             number = Math.max(cmd.length-1, 0);
-        if (text == 'source' || text == 'src') {
-        write_output('<div class="highlight"><span class="ow">Source: https://bitbucket.org/ilovelinux/pyline/</span></div>');
-        }
-        else if (text == 'resetall') {
-                $.getJSON(document.location.origin+"/exec/", {"restore":"True"}, function(json){
-                    if (json.restored) {
-                    cmd = [];
-                    number = 0;
-                    write_output('<div class="highlight"><span class="ow">Restored: ' + json.restored + '</span></div>', true);
-                    }
+            if (text == 'source' || text == 'src') {
+            write_output('<div class="highlight"><span class="ow">Source: https://bitbucket.org/ilovelinux/pyline/</span></div>');
+            }
+            else if (text == 'resetall') {
+                    $.getJSON(document.location.origin+"/exec/", {"restore":"True"}, function(json){
+                        if (json.restored) {
+                        cmd = [];
+                        number = 0;
+                        write_output('<div class="highlight"><span class="ow">Restored: ' + json.restored + '</span></div>', true);
+                        }
+                    });
+                }
+            else if (text == 'about') {
+                $.getJSON(document.location.origin+"/exec/", {"exec":"sys.version.split()[0]"}, function(json){
+                    var version = json.execute.replace("'", "").replace("'", "");
+                    write_output('<div class="highlight"><span class="ow">Author: Antonio Spadaro < antoniospadaro45 AT gmail DOT com ><br>Source: https://bitbucket.org/ilovelinux/pyline/<br>Based on Python '+version+'</span></div>');
                 });
             }
-        else if (text == 'about') {
-            $.getJSON(document.location.origin+"/exec/", {"exec":"sys.version.split()[0]"}, function(json){
-                var version = json.execute.replace("'", "").replace("'", "");
-                write_output('<div class="highlight"><span class="ow">Author: Antonio Spadaro < antoniospadaro45 AT gmail DOT com ><br>Source: https://bitbucket.org/ilovelinux/pyline/<br>Based on Python '+version+'</span></div>');
-            });
-        }
             else {
                 $.getJSON(document.location.origin+"/exec/", {'exec':text}, function(json){
                     write_output(json.code);
